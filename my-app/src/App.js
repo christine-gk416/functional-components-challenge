0340d4e7a26016f227e2e0c8bc24507547c87bc3
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-18 14:17:53 +0000

    complete class componenets challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,21 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar.js'
+import NavBarSimple from './components/NavBarSimple'
 
 function App() {
-  return (
-    <div className={css.App}>
-      {<Sidebar/>}
-    </div>
-  );
+    return ( <
+        div className = {
+            css.App
+        } > {
+            <
+            Sidebar / >
+
+        } <
+        NavBarSimple / >
+
+        <
+        /div>
+    );
 }
 
 export default App;
