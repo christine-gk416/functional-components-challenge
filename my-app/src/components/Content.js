3d7e83401ebcd197fddb6524432b6d35b491bf59
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-20 22:09:12 +0000

    Complete component lifecycle challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import {savedPosts} from '../posts.json'
 import Posts from './Posts';
+import Loader from './Loader';
 
 export class Content extends Component {
 
@@ -9,16 +10,32 @@ export class Content extends Component {
       super(props)
     
       this.state = {
-         
+         isLoaded: false
       }
     }
+
+    componentDidMount () {
+      setTimeout (() => {
+        this.setState ({
+          isLoaded: true
+        })
+      }, 2000
+
+      )
+    }
+
   render() {
      return (
       <div>
+          {/* <Loader/> */}
           <div>
             <h1 className={css.TitleBar}>My Photos</h1>
           </div>
           <div className={css.SearchResults}>
+            
+          {this.state.isLoaded ? (<Posts savedPosts={savedPosts}/>) :
+            (<Loader/>)}
+
             {/* {savedPosts.map((post)=>
             {
                 return (
@@ -34,7 +51,8 @@ export class Content extends Component {
             )
             
             } */}
-            <Posts savedPosts={savedPosts}/>
+
+            {/* <Posts savedPosts={savedPosts}/> */}
           </div>
       </div>
     )
